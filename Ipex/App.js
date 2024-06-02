2f93b35475bf2b2fa4ba20f118146f83387a53d2
aggreypaintsil168 <ipaggrey@st.ug.edu.gh>
2024-06-02 15:38:09 +0000

    started copy

---
+++ Ipex/App.js
@@ -0,0 +1,38 @@
+// App.js
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import Header from './Components/Header';
+import Categories from './Components/Categories';
+import OngoingTask from './Components/OngoingTask';
+
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.container}>
+          <Header />
+          <Categories searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+          <OngoingTask />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#F7F0E8',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#F7F0E8',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollView: {
+    marginHorizontal: 0,
+  },
+});
---
+++ Ipex/Components/Categories.js
@@ -0,0 +1,143 @@
+// Categories.js
+import React, { useState } from 'react';
+import { View, Text, ScrollView, Image, TextInput, StyleSheet } from 'react-native';
+
+const Categories = ({ searchQuery, setSearchQuery }) => {
+  return (
+    <>
+      <View style={styles.Searchlane}>
+        <View style={styles.SearchField}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Search"
+            value={searchQuery}
+            onChangeText={text => setSearchQuery(text)}
+          />
+        </View>
+        <View style={styles.VectorView}>
+          <Image source={require('../assets/Vector.png')} style={styles.Vector} />
+        </View>
+      </View>
+      <View style={styles.CategoriesText}>
+        <Text style={styles.categories}>Categories</Text>
+      </View>
+      <View style={styles.Categorieslayout}>
+        <ScrollView horizontal={true} style={styles.scrollView}>
+          <View style={styles.CategoryView}>
+            
+          </View>
+          
+          <View style={styles.CategoryView}>
+            <Text>Study</Text>
+            <Text>12 Tasks</Text>
+            <Image source={require('../assets/WorkingOnline.png')} style={styles.Vector} />
+          </View>
+          
+          <View style={styles.CategoryView}>
+            <Text>Study</Text>
+            <Text>12 Tasks</Text>
+            <Image source={require('../assets/WorkingTask.png')} style={styles.Vector} />
+          </View>
+          
+          <View style={styles.CategoryView}>
+            <Text>Study</Text>
+            <Text>10 Tasks</Text>
+            <Image source={require('../assets/WeThree.png')} style={styles.Vector} />
+          </View>
+          
+          <View style={styles.CategoryView}>
+            <Text>Study</Text>
+            <Text>10 Tasks</Text>
+            <Image source={require('../assets/Around.png')} style={styles.Vector} />
+          </View>
+          
+          <View style={styles.CategoryView}>
+            <Text>Exercise</Text>
+            <Text>8 Tasks</Text>
+            <Image source={require('../assets/Studies.png')} style={styles.Vector} />
+          </View>
+    
+          <View style={styles.CategoryView}>
+            <Text>Study</Text>
+            <Text>6 Tasks</Text>
+            <Image source={require('../assets/Time.png')} style={styles.Vector} />
+          </View>
+          
+          <View style={styles.CategoryView}>
+            <Text>Study</Text>
+            <Text>4 Tasks</Text>
+            <Image source={require('../assets/Together.png')} style={styles.Vector} />
+          </View>
+          
+          <View style={styles.CategoryView}>
+            <Text>Exercise</Text>
+            <Text>2 Tasks</Text>
+            <Image source={require('../assets/Exercise.png')} style={styles.Vector} />
+          </View>
+           
+
+        </ScrollView>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  Searchlane: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    paddingHorizontal: 5,
+    marginBottom: 5,
+  },
+  searchInput: {
+    height: 50,
+    borderColor: '#fff',
+    borderWidth: 1,
+    borderRadius: 8,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+    width: '100%',
+  },
+  SearchField: {
+    flex: 1,
+    marginLeft: 15,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+  },
+  VectorView: {
+    borderRadius: 15,
+    backgroundColor: '#F0522F',
+    padding: 10,
+    marginLeft: 22,
+    marginRight: 19,
+  },
+  categories: {
+    fontWeight: '700',
+    fontSize: 20,
+    textAlign: 'right',
+  },
+  CategoriesText: {
+    alignSelf: 'right',
+    marginLeft: 15,
+    marginTop: 30,
+  },
+  CategoryView: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 15,
+    margin: 10,
+  },
+  Categorieslayout: {
+    flexDirection: 'row',
+  },
+  scrollView: {
+    marginHorizontal: 0,
+  },
+  Vector: {
+    
+  },
+});
+
+export default Categories;
